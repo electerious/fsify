c4b5bba3cbc3c6005631a836a8e0153cd2674e8c
Tobias Reich <tobias.reich.ich@gmail.com>
2017-02-20 10:19:15 +0100

    bin test

---
+++ test/bin.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const assert = require('chai').assert
+const bin    = require('./../src/bin')
+
+describe.only('bin()', function() {
+
+	it('should return a function', function() {
+
+		assert.isFunction(bin())
+
+	})
+
+	it('should add multiple items and return them', function() {
+
+		const _bin = bin()
+
+		assert.deepEqual([], _bin())
+		assert.deepEqual([ 1 ], _bin(1))
+		assert.deepEqual([ 1, 2 ], _bin(2))
+		assert.deepEqual([ 1, 2, 3, 4 ], _bin(3, 4))
+
+	})
+
+})
